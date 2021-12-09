0e1197dab8feca742844bd9a6b5a225682815a7f
zoebstephens <zoebstephens@gmail.com>
2021-12-09 00:05:51 -0800

    Add no armor button

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    UnityEngine.U
+   public SpriteRenderer ArmorRenderer;
+    
+    public void SetArmor(Sprite newSprite)
+    {
+        ArmorRenderer.sprite = newSprite;
+    }
     // Start is called before the first frame update
     void Start()
     {
