344217351dbfb94bb8d7232ba24264ae1cd7b617
zoebstephens <zoebstephens@gmail.com>
2021-12-13 10:44:34 -0800

    Random Button Work

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    public SpriteRenderer ArmorRenderer;
+   public List<UnityEngine.UI.Button> WeaponButtons;
+
+   public List<UnityEngine.UI.Button> ArmorButtons;
     
     public void SetArmor(Sprite newSprite)
     {
@@ -15,6 +18,16 @@ public class PlayerController : MonoBehaviour
     public void SetWeapon(Sprite newSprite)
     {
         WeaponRenderer.sprite = newSprite;
+    }
+
+    public void SetRandomWeapon()
+    { 
+        WeaponButtons[Random.Range(0, 7)].onClick.Invoke();
+    }
+
+    public void SetRandomArmor()
+    { 
+        ArmorButtons[Random.Range(0,6)].onClick.Invoke();
     }
 
     // Start is called before the first frame update
